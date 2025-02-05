746fa1e052ccaa59cdf3729cbb0831d6b48800e8
Naveen Shetty <naveen@creaza.com>
2025-02-05 17:14:13 +0100

    Cross issue

--- backend-service/src/start_api.py
+++ backend-service/src/start_api.py
@@ -4,4 +4,4 @@ app = create_app()
 CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all routes
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
